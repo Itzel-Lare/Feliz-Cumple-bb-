6402b811d9dee10d3e8207ec91506ac1c0c04adf
Itzel-Lare <163046969+Itzel-Lare@users.noreply.github.com>
2025-02-19 16:31:18 -0600

    script.js

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ class Birthday {
   
   resize() {
     this.width = canvas.width = window.innerWidth
+    this.height = canvas.height = window.innerHeight
     let center = this.width / 2 | 0
     this.spawnA = center - center / 4 | 0
     this.spawnB = center + center / 4 | 0
     
-    this.height = canvas.height = window.innerHeight
     this.spawnC = this.height * .1
     this.spawnD = this.height * .5
   }
